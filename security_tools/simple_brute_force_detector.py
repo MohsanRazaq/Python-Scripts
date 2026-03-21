98420c1b1131cff066591161f80d0640f475c123
MohsanRazaq <mohsansec@gmail.com>
2026-03-21 22:11:34 +0500

    .

---
+++ security_tools/simple_brute_force_detector.py
@@ -0,0 +1,17 @@
+# Dictionary to store login attempts
+attempts = {}
+
+ip = "192.168.1.1"
+
+# Simulate 5 login attempts
+for i in range(5):
+    
+    # Increase attempt count
+    attempts[ip] = attempts.get(ip, 0) + 1
+    
+    # Check if blocked
+    if attempts[ip] > 3:
+        print("IP blocked:", ip)
+        break
+    else:
+        print("Login failed attempt", attempts[ip])
