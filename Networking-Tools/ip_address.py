087c6c04083e6e0f88e779d58ca4aacd4a65c16a
MohsanRazaq <mohsansec@gmail.com>
2026-03-24 23:12:42 +0500

    simple script to know how many  targets are  in range

---
+++ Networking-Tools/ip_address.py
@@ -0,0 +1,8 @@
+import ipaddress
+## How many targets are in range of given IP
+ip='192.168.100.0/30'
+network=ipaddress.ip_network(ip)
+print(f'The network is : {network}\n')
+
+for ip in network:
+    print(f'Target ip is : {ip}')
